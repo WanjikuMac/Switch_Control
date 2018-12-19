b46f02d71532f9bf53c260ee2dc52622a04d21ce
WanjikuMac <edwardscaroline32@gmail.com>
2018-12-19 10:20:39 +0300

    switch control

---
+++ lib/switch_control.ex
@@ -0,0 +1,18 @@
+defmodule SwitchControl do
+  @moduledoc """
+  Documentation for SwitchControl.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SwitchControl.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/switch_control_test.exs
@@ -0,0 +1,8 @@
+defmodule SwitchControlTest do
+  use ExUnit.Case
+  doctest SwitchControl
+
+  test "greets the world" do
+    assert SwitchControl.hello() == :world
+  end
+end
